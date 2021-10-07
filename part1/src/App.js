c7cd7a5dbeda91abbd818f91dd0607c597090606
Jonathan Dockwiller <47151764+jonathandock@users.noreply.github.com>
2021-10-07 15:27:13 +0200

    part1 - 1.2 - Part component

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,21 +5,19 @@ const Header = (props) => (
 )
 
 const Content = (props) => (
-  <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </>
+  <div>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
+  </div>
 )
 
 const Total = (props) => (
   <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+)
+
+const Part = (props) => (
+  <p>{props.part} {props.exercises}</p>
 )
 
 const App = () => {
